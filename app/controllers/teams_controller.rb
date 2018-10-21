f5b6f3dae914d5bae8b976a9dcbed4296de9094f
Mizuki Zenta <mizukisb@gmail.com>
2018-10-21 16:06:44 +1100

    add team crud system

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,9 +3,50 @@ class TeamsController < ApplicationController
 
   
   def index
+    @teams = Team.all
   end
 
-  private 
+  def new
+    @team = Team.new
+  end
+
+  def create 
+    @team =Team.create(team_params)
+
+    if @team.save
+      redirect_to teams_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @team =Team.find(params[:id])
+  end
+
+  def update
+    @team =Team.find(params[:id])
+
+    if @team.update(team_params)
+      redirect_to teams_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @team =Team.find(params[:id])
+
+    @team.destroy
+    redirect_to teams_path
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def team_params
+    params.require(:team).permit(:name)
+  end
+
   def current_user_validation
     redirect_to root_path if !session[:current_user]
   end
